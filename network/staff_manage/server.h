af73827e0c8258209bbe372b9de8769e98dea609
Jarvis03 <guoxuan0911@163.com>
2019-10-25 17:22:19 +0800

    client

---
+++ network/staff_manage/server.h
@@ -0,0 +1,33 @@
+
+#include <stdint.h>
+// Do not use magic number
+// Data length define
+#define PHONE_LEN     20
+#define NAME_LEN      20
+#define ADDR_LEN      30
+#define PASSWORD_LEN  15
+
+#define SERVER_HOST_ADDR  "192.168.3.107"
+/*
+ * staff information
+ */
+struct staff_info {
+    uint32_t number;
+    char     phone[PHONE_LEN];
+    char     name[NAME_LEN];
+    char     addr[ADDR_LEN];
+    char     age;
+    int      level;
+    double   salary;
+};
+
+struct user {
+    char  flag;
+    char  name[NAME_LEN];
+    char  psword[PASSWORD_LEN];
+};
+
+//interface
+
+
+
